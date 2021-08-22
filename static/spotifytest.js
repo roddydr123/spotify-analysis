60b511834dd8cac8613855034f904a05c3d49cc5
roddydr123 <davidroddy321@gmail.com>
2021-08-22 20:48:26 +0100

    deleting useless js file

--- static/spotifytest.js
+++
@@ -1,9 +0,0 @@
-/*
-trying it in js
-*/
-
-function dewit()    {
-    $.get("https://api.spotify.com/v1/albums/104", function response(data, status)  {
-        document.getElementById("stuff").innerHTML = "hahahaha";
-    });
-};
